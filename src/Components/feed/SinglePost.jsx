adfede9cf1e5a78421bcc4ee997ce2a5eeb1f771
Engr. Md. Muslim Uddin Arju <mdarju240@gmail.com>
2021-09-10 10:05:11 +0100

    add edit delete options for single post

--- src/Components/feed/SinglePost.jsx
+++ src/Components/feed/SinglePost.jsx
@@ -1,37 +1,101 @@
 /* eslint-disable jsx-a11y/img-redundant-alt */
-import { Card, Col, Form, Row } from "react-bootstrap";
+import { Card, Col, Form, Row,Dropdown } from "react-bootstrap";
 import { FaPaperPlane } from "react-icons/fa";
 import { BiWorld, BiLike, BiComment, BiShare } from "react-icons/bi";
 import { useState } from "react";
 import axios from "axios";
-
+import CenteredEditModal from "./EditPostModal";
+import EditCommentModal from './EditCommentModal'
 
 const SinglePost = (props) => {
-  
+  console.log(props.post)
+  const id=localStorage.getItem('id')
   const [showComment, SetShowComment] = useState(false);
+  const [modalShow, setModalShow] = useState(false);
+  const [EditCommentmodalShow, setEditCommentmodalShow] = useState(false);
+  const [comment, setComment] = useState({
+    comment: "",
+    profileId:`${id}`,
+  });
   const userID = props.user._id
-  const id=localStorage.getItem('id');
+  const curent_userId=localStorage.getItem('id');
   const accesstoken=localStorage.getItem('accesstoken');
-  
+  const avatar=localStorage.getItem('avatar');
+
   const LikeAction=async (postId)=>{
-    const response= await axios.get(`${process.env.REACT_APP_API_URL}/post/${postId}/like/${id}`,{
+    const response= await axios.get(`${process.env.REACT_APP_API_URL}/post/${postId}/like/${curent_userId}`,{
       headers: {
       'authentication':  `${accesstoken}`
       }
     })
     if(response){
-      console.log(response)
+      console.log('like',response.data)
+      setTimeout(function() {
+        window.location.replace('/');
+      }, 500);
     }else{
       console.log("something wrong")
     }
   }
+  const onSubmit=async (e,postId) =>{
+    e.preventDefault();
+    console.log(comment)
+    const response= await axios.post(`${process.env.REACT_APP_API_URL}/post/${postId}/comment`,comment, {
+      headers: {
+      'authentication':  `${accesstoken}`
+      }
+    })
+    if(response){
+      setTimeout(function() {
+        window.location.replace('/');
+      }, 500);
+    }else{
+      console.log("something wrong")
+    }
+   
+  }
+  const onInputChange = e => {
+    setComment({ ...comment, [e.target.name]: e.target.value });
+    e.preventDefault();
+    if(e.key === 'Enter'){
+      console.log('enter pressed')
+    }
+  };
+ const DeletePost =async (postId) =>{
+  const response= await axios.delete(`${process.env.REACT_APP_API_URL}/post/${postId}`,{
+    headers: {
+    'authentication':  `${accesstoken}`
+    }
+  })
+  if(response){
+    props.fetch()
+  }else{
+    console.log("something wrong")
+  }
+ }
+ 
 
-
+ const DeleteComment =async (postId,commentId) =>{
+  const response= await axios.delete(`${process.env.REACT_APP_API_URL}/post/${postId}/comment/${commentId}`,{
+    headers: {
+    'authentication':  `${accesstoken}`
+    }
+  })
+  if(response){
+    setTimeout(function() {
+      window.location.replace('/');
+    }, 500);
+  }else{
+    console.log("something wrong")
+  }
+ }
 
   return (
+    
     <Card className="w-100">
       <Card.Body className="post-card">
-        <a href={userID} className="post-user-info" style={{"color":"#191919"}}>
+        <div style={{display: "flex",justifyContent: "space-between"}}>
+        <a href={`/profile/${userID}`} className="post-user-info" style={{"color":"#191919"}}>
           <div className="user-icon-container">
             <img src={props.user.image} alt="" className="user-icon" width="45" height="45"
             />
@@ -39,6 +103,7 @@ const SinglePost = (props) => {
           <div className="user-fragment-info fragment-info-post">
             <h6 className="post-user-name">
               {props.user.name + " " + props.user.surname} • <span className="text-secondary">{relatedness()}</span>
+            
             </h6>
             <p className="post-job-descriptor text-secondary">
               {props.user.title} 
@@ -48,6 +113,26 @@ const SinglePost = (props) => {
             </p>
           </div>
         </a>
+        <span>
+        <Dropdown style={{marginTop:"15px"}}>
+        <Dropdown.Toggle variant="" style={{background:"none", border:"none", fontSize: "12px", padding:"0px !important"}} id="dropdown-basic">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" width="24" height="24" focusable="false">
+          <path d="M14 12a2 2 0 11-2-2 2 2 0 012 2zM4 10a2 2 0 102 2 2 2 0 00-2-2zm16 0a2 2 0 102 2 2 2 0 00-2-2z"></path>
+        </svg>
+        </Dropdown.Toggle>
+        <Dropdown.Menu>
+          <Dropdown.Item href={`/profile/${props.post.user._id}`}>View Profile</Dropdown.Item>
+          {props.post.user._id===curent_userId ? <Dropdown.Item  onClick={() => setModalShow(true)}>Edit Post</Dropdown.Item>: <></>}
+          <CenteredEditModal
+              show={modalShow}
+              post={props.post}
+              onHide={() => setModalShow(false)}
+            />
+          {props.post.user._id===curent_userId ? <Dropdown.Item  onClick={() => DeletePost(props.post._id)}>Delete</Dropdown.Item>: <></>}
+        </Dropdown.Menu>
+        </Dropdown>
+        </span>
+        </div>
         <p className="user-post">
           {props.post.text}
         </p>
@@ -89,7 +174,7 @@ const SinglePost = (props) => {
         <Row>
           <Col className={showComment ? "post-comment-area" : "d-none"}>
           {props.post.Comments.map(c => 
-             <Row className="d-flex">
+             <Row className="d-flex justify-content-between">
              <div className="post-comment-img">
                <img
                  src={c.profileId.image}
@@ -98,8 +183,29 @@ const SinglePost = (props) => {
                  style={{height: "40px", width: "40px", padding: "5px"}}
                />
              </div>
-             <div className="pt-2">
+             <div className="pt-2 mr-auto">
              {c.comment}
+             </div>
+             <div className="pt-2">
+             {c.profileId._id===curent_userId ? 
+             <Dropdown>
+              <Dropdown.Toggle variant="" style={{background:"none", border:"none", fontSize: "12px", padding:"0px !important"}} id="dropdown-basic">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" width="24" height="24" focusable="false">
+                <path d="M14 12a2 2 0 11-2-2 2 2 0 012 2zM4 10a2 2 0 102 2 2 2 0 00-2-2zm16 0a2 2 0 102 2 2 2 0 00-2-2z"></path>
+              </svg>
+              </Dropdown.Toggle>
+              <Dropdown.Menu>
+              <Dropdown.Item onClick={() => setEditCommentmodalShow(true)}>Edit Comment</Dropdown.Item>
+              <EditCommentModal
+              show={EditCommentmodalShow}
+              comment={c}
+              post={props.post}
+              onHide={() => setModalShow(false)}
+            />
+              <Dropdown.Item  onClick={() => DeleteComment(props.post._id, c._id)}>Delete Comment</Dropdown.Item>
+              </Dropdown.Menu>
+              </Dropdown>
+              : <></>}
              </div>
            </Row>
             
@@ -108,24 +214,27 @@ const SinglePost = (props) => {
             <Row className="d-flex">
               <div className="post-comment-img">
                 <img
-                  src={props.user.image}
+                  src={avatar}
+                  style={{height: "40px", marginTop: "5px"}}
                   alt="profile-picture"
                   className="rounded-circle"
                 />
               </div>
               <div className="post-textarea">
-                <Form className="post-textarea w-100">
-                  <Form.Group
+               <Form onSubmit={(e,postId )=> onSubmit(e,props.post._id)}>
+               <Form.Group
                     className="mb-2 mt-2 pr-2"
                     controlId="exampleForm.ControlTextarea1"
                   >
                     <Form.Control
-                      as="textarea"
-                      rows={1}
-                      className="post-comment-textarea w-100"
+                      type="text"
+                      name="comment"
+                      defaultValue={comment.comment}
+                      className="w-100"
+                      onChange={e => onInputChange(e)}
                     />
-                  </Form.Group>
-                </Form>
+                </Form.Group>
+               </Form>
               </div>
             </Row>
           </Col>
